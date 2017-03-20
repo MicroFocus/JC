27055f07d1209949ffd709dd384097997dcc50c6
Yuval Koren <yuval.koren@hp.com>
2017-03-20 21:16:00 +0200

    contain skeleton of JC framework

---
+++ src/main/java/com/hpe/JC.java
@@ -0,0 +1,19 @@
+package com.hpe;
+
+/**
+ * Created by koreny on 3/20/2017.
+ */
+public class JC {
+    public static void Given(String step) {
+        StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+    }
+
+    public static void When(String step) {
+
+    }
+
+    public static void Then(String step) {
+
+    }
+
+}
---
+++ src/main/java/com/hpe/Scenario.java
@@ -0,0 +1,8 @@
+package com.hpe;
+
+/**
+ * Created by koreny on 3/20/2017.
+ */
+public @interface Scenario {
+    String value() default "";
+}
